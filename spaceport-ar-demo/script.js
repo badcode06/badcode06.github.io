f531b5ef0fcf4c7ea738cc4460c6539d8128e549
badcode06 <badcodecpp@gmail.com>
2022-04-18 13:41:06 -0700

    test with chialab

--- spaceport-ar-demo/script.js
+++ spaceport-ar-demo/script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.05 0.05 0.05');
+        model.setAttribute('scale', '0.01 0.01 0.01');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
